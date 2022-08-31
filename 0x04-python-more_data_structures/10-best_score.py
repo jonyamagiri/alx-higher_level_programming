9cca2fbbafceda908e8bb96bbc3adc10ebb1f1c0
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-08-31 18:18:30 +0300

    fixed pycodestyle requirement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(a_dictionary.items(), key = lambda item: item[1])[0]
+    return max(a_dictionary.items(), key=lambda item: item[1])[0]
 
 
 '''
