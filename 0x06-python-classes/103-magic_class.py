30632e7b007cc84420fcdda1c9f39ab6eecebf9a
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-14 19:52:44 +0300

    ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Defines class MagicClass.
+"""
+import math
+
+
+class MagicClass:
+    """
+    Initializes and defines the methods area and circumference.
+    """
+
+    def __init__(self, radius=0):
+        """
+        Initializes the MagicClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """
+        Calculates and returns area of the MagicClass.
+        """
+        return (self.__radius ** 2 * math.pi)
+
+    def area(self):
+        """
+        Calculates and returns circumference of the MagicClass.
+        """
+        return (2 * math.pi * self.__radius)
