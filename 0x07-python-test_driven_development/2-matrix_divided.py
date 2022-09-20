48d78257e605934935cac21c93de1bde1016c2b3
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-20 19:19:04 +0300

    function that divides all elements of a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,12 +7,15 @@ Divides all elements of a matrix and returns new matrix.
 
 def matrix_divided(matrix, div):
     """
-    Adds 2 integers (a and b) that are integers or floats.
-    a and b first casted to integers if they are float.
-    Returns an integer: the addition of a and b.
+    Accepts a matrix that is a list (lists of integers or floats),
+    and a div that must be a number (integer or float).
+    All elements of matrix are divided by div, rounded to 2 decimal places
+    Returns a new matrix (the result).
     """
     new_matrix = []
-    
+    msg_1 = "Each row of the matrix must have the same size"
+    msg_2 = "matrix must be a matrix (list of lists) of integers/floats"
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
@@ -21,13 +24,13 @@ def matrix_divided(matrix, div):
 
     for lists in matrix:
         if len(lists) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(msg_1)
         second_list = []
         for items in lists:
             if not isinstance(items, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(msg_2)
             else:
                 second_list.append(round(items / div, 2))
         new_matrix.append(second_list)
-        
+
     return new_matrix
