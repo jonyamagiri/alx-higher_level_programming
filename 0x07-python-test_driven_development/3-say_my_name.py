51071024a894876c39f490aa1e190afeea68f3d4
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-23 18:12:02 +0300

    fixed TypeError bug

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,9 +14,9 @@ def say_my_name(first_name, last_name=""):
         return None
 
     if type(first_name) != str:
-        raise TypeError("first_name must be a string ")
+        raise TypeError("first_name must be a string")
 
     if type(last_name) != str:
-        raise TypeError("last_name must be a string ")
+        raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
