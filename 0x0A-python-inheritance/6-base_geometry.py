4cabedddac913b5f74399343d790617e812d8d82
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-26 17:28:47 +0300

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry.py having class BaseGeometry with method area(self).
+"""
+
+
+class BaseGeometry:
+    """
+    Defines class BaseGeometry.
+    Methods:
+        area(self)
+    Exceptions:
+        area() is not implemented
+    """
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
