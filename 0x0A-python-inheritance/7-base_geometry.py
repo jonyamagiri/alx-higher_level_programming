d9320983cdd27388231e7407f597e5f842f02e8f
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-26 18:43:39 +0300

    fixed bug - checked tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,7 +14,8 @@ class BaseGeometry:
     def area(self):
         """
         Not implemented.
-        Exceptions: area() is not implemented
+        Exceptions:
+            area() is not implemented
         """
         raise Exception("area() is not implemented")
 
@@ -28,7 +29,7 @@ class BaseGeometry:
             TypeError - if value is not an integer
             ValueError - if value is less or equal to 0
         """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
