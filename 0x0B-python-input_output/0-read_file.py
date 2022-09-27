b7021f22d50d6d56e9edcc2bd4f23fc3d523e4f6
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-27 16:21:25 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ Module 0-read_file.py with method read_file.
 
 def read_file(filename=""):
     """
-    Accepts textfile filename.
+    Accepts textfile (filename).
     Reads the text file (UTF8) and prints it to stdout.
     """
     with open(filename, mode="r", encoding="utf-8") as f:
