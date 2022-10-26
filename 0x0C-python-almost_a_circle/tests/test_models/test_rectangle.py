ec46412674c1e4ba4f14d0ddebbc9e3ee2d9653c
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-10-26 20:43:55 +0300

    fixed bug in tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -44,13 +44,13 @@ class TestRectangle(unittest.TestCase):
             rec_5 = Rectangle(float('inf'))
             rec_6 = Rectangle(9.5, 9.3)
             rec_7 = Rectangle(-8, 9)
-            rec_3 = Rectangle()
+            rec_8 = Rectangle()
 
         self.assertEqual(rec_1.id, 1)
         self.assertEqual(rec_1._Base__nb_objects, 1)
         self.assertEqual(rec_2.id, 12)
         self.assertEqual(rec_2._Base__nb_objects, 1)
-        
+
     def test_area(self):
         """Tests area()"""
         rec_1 = Rectangle(3, 2)
@@ -68,10 +68,11 @@ class TestRectangle(unittest.TestCase):
             rec_1.display()
             self.assertEqual(fakeOutput.getvalue(), '###\n###\n')
 
-        rec_2 = Rectangle(8, 7, 0, 0, 12)
+        rec_2 = Rectangle(5, 3, 1, 2)
         with patch('sys.stdout', new=StringIO()) as fakeOutput:
             rec_2.display()
-            self.assertEqual(fakeOutput.getvalue(), '########\n########\n########\n########\n########\n########\n########\n')
+            self.assertEqual(
+                fakeOutput.getvalue(), '\n\n #####\n #####\n #####\n')
 
     def test_str(self):
         """Tests __str__()"""
@@ -84,7 +85,6 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rec_2.__str__(), '[Rectangle] (12) 0/0 - 8/7')
         self.assertEqual(rec_3.__str__(), '[Rectangle] (2) 1/0 - 6/2')
         self.assertEqual(rec_4.__str__(), '[Rectangle] (engineer) 0/0 - 3/2')
-
 
     def test_update(self):
         """Tests update()"""
@@ -103,7 +103,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rec_4.__str__(), '[Rectangle] (None) 0/0 - 3/2')
         rec_5.update(-9)
         self.assertEqual(rec_5.__str__(), '[Rectangle] (-9) 0/0 - 3/2')
-        
+
     def test_to_dictionary(self):
         """Tests to_dictionary()"""
 
@@ -116,7 +116,7 @@ class TestRectangle(unittest.TestCase):
         dict_2 = {'id': 12, 'width': 8, 'height': 7, 'x': 0, 'y': 0}
         dict_3 = {'id': 2, 'width': 6, 'height': 2, 'x': 1, 'y': 0}
         dict_4 = {'id': 'engineer', 'width': 3, 'height': 2, 'x': 0, 'y': 0}
-        
+
         self.assertDictEqual(rec_1.to_dictionary(), dict_1)
         self.assertDictEqual(rec_2.to_dictionary(), dict_2)
         self.assertDictEqual(rec_3.to_dictionary(), dict_3)
