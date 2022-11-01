f1742ecf8017db9e7c39b9162a35f1e2ec72364f
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-01 21:57:33 +0300

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates table second_table in database hbtn_0c_0 and adds multiples rows.
+-- second_table has id INT, name VARCHAR(256) and score INT
+-- database name passed as argument of mysql command
+-- if second_table exists, script should not fail. No SELECT and SHOW statements
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT, 
+    name VARCHAR(256),
+    score INT);
+INSERT INTO second_table(id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
