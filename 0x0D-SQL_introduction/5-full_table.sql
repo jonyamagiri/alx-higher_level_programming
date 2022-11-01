6eae23d930057a8e2e426fdc5a2e8ab2f3b3d430
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-01 18:52:45 +0300

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Prints the full description of the table first_table from the database
+-- hbtn_0c_0 in MySQL server. No DESCRIBE or EXPLAIN statements.
+-- database name passed as argument of mysql command. 
+
+SHOW CREATE TABLE first_table;
