28449bbdbc7f6756c5d8b5be75f01feda263b7c2
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-01 22:48:31 +0300

    Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Computes score average of all records in second_table of database hbtn_0c_0
+-- database name passed as argument of mysql command
+
+SELECT AVG(score) AS 'average'
+FROM second_table;
