4ca1bb0c0c17af5d3ee582fed8c87f596d36bd2c
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-01 22:37:05 +0300

    Cheating is bad

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Updates the score of Bob to 10 in the table second_table
+-- no use of Bob’s id value, only the name field
+-- database name passed as argument of mysql command
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob'
