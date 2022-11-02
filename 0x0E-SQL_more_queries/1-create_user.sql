0246b42e3fecb52d1efb17a75c1e8a1385a8e91e
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-02 23:44:04 +0300

    Root user fixed styling

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,10 @@
 -- user_0d_1 has all privileges, with password set to user_0d_1_pwd
 -- if user user_0d_1 already exists, script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
