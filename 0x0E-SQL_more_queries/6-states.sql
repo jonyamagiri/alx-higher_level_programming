23791bffa1ca539afd25b00d152e059d1f072dc8
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 00:30:38 +0300

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- states has id INT unique, auto generated, can’t be null and is a primary key;
+name VARCHAR(256) can’t be null
+-- if database hbtn_0d_usa already exists, script should not fail
+-- if table states already exists, script should not fail
+-- database name passed as an argument of the mysql command
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    PRIMARY KEY (id),
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
