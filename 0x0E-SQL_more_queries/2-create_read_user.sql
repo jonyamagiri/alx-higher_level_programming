ba9a0c62dd0fc66c952ab014ea92c40b16d1a9bd
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-02 23:51:08 +0300

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- if database hbtn_0d_2 already exists, script should not fail
+-- if user user_0d_2 already exists, script should not fail
+
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_2
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+    ON 'hbtn_0d_2'.*
+    TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
