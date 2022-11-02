3debb76519beb60bd9bd917b74b377cc220bd154
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 00:20:57 +0300

    Unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- Creates the table unique_id
+-- unique_id has id INT with the default value 1 and must be unique name VARCHAR(256)
+-- if table unique_id already exists, script should not fail
+-- database name passed as an argument of the mysql command
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
