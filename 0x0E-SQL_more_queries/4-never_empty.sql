11af67e022830fab987c6d380f8fd7f24230e653
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 15:54:15 +0300

    ID cant be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,9 @@
--- Creates the table id_not_null 
+-- Creates the table id_not_null in database hbtn_0d_2
 -- id_not_null has id INT with the default value 1, name VARCHAR(256)
 -- if table id_not_null already exists, script should not fail
 -- database name passed as an argument of the mysql command
 
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS id_not_null(
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
