3a4f8298d410741fb374b8844db0ba9242e1961a
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 15:48:52 +0300

    Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,9 @@
--- Creates the table force_name
+-- Creates the table force_name in database hbtn_0d_2
 -- force_name has id INT, name VARCHAR(256) can’t be null
 -- if table force_name already exists, script should not fail
 -- database name passed as an argument of the mysql command
 
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS force_name(
     id INT,
     name VARCHAR(256) NOT NULL
 );
