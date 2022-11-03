3480bf4fb61692daa9f047e6dab5ccae2b894a3b
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 13:49:32 +0300

    Temperatures 0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,5 @@
 -- Import in hbtn_0c_0 database the table dump (temperatures.sql)
+-- to import: $ curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql" -s | mysql -uroot -p hbtn_0c_0
 -- display average temperature (Fahrenheit) by city ordered by temperature (descending)
 
 SELECT city, AVG(value) AS 'avg_temp'
