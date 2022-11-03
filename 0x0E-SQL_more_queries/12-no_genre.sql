a64b617fd97e62463830725eb69ea3f0c7295e5e
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-03 23:05:32 +0300

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Import the database dump from hbtn_0d_tvshows
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- display: tv_shows.title - tv_show_genres.genre_id
+-- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id; use only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
