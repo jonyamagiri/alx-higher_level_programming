69a310b252d4b5456ce4ff47a69a663cb5569794
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:06:59 +0300

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,7 +5,12 @@
 -- display: tv_shows.title - tv_show_genres.genre_id
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id; use only one SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id ASC;
