6ca493ca3291d5823c50289c9cd149ab265f9a2e
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:12:49 +0300

    Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,7 +4,12 @@
 -- If a show doesn’t have a genre, display NULL
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id; use only one SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id ASC;
