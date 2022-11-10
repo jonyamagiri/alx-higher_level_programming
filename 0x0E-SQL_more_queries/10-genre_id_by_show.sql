87b2f6d03e8aa94366afdfd929ac6e92cf3ec684
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:10:39 +0300

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,8 @@
 -- Import the database dump from hbtn_0d_tvshows
--- to import: $ echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
--- $ curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql" -s | mysql -uroot -p hbtn_0d_tvshows
+-- URL: https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- to import: (create database then use 'curl' to download)
+-- 1. $ echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+-- 2. $ curl "URL" -s | mysql -uroot -p hbtn_0d_tvshows
 -- lists all shows in hbtn_0d_tvshows that have at least one genre linked
 -- display: tv_shows.title - tv_show_genres.genre_id
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id; use only one SELECT statement
