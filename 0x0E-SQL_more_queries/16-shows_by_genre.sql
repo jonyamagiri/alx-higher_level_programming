bff1581ba2634e12634f64c2a0535ddcc99f649b
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:17:26 +0300

    List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,8 +4,13 @@
 -- display: tv_shows.title - tv_genres.name
 -- sorted in ascending order by the show title and genre name; use only one SELECT statement
 
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
+SELECT
+    tv_shows.title,
+    tv_genres.name
+FROM
+    tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+ORDER BY
+    tv_shows.title,
+    tv_genres.name ASC;
