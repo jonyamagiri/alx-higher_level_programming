5b3b9bdf53ddb3811f05d682a1848198a37eb723
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:16:00 +0300

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,9 +3,13 @@
 -- display: tv_genres.name
 -- sorted in ascending order by the genre name; use only one SELECT statement
 
-SELECT tv_genres.name
-FROM tv_genres
+SELECT
+    tv_genres.name
+FROM
+    tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name ASC;
+WHERE
+    tv_shows.title = "Dexter"
+ORDER BY
+    tv_genres.name ASC;
