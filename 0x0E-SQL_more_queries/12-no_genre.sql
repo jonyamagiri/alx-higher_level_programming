450ad9d1ac6391db0f502cef4ff244501ed5b593
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-10 16:13:39 +0300

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,8 +3,14 @@
 -- display: tv_shows.title - tv_show_genres.genre_id
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id; use only one SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+WHERE
+    tv_show_genres.genre_id IS NULL
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id ASC;
