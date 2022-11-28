8b584c86b96831d1ca9cac6923e199a6b2176d8a
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-11-29 01:32:17 +0300

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+}
+const myNum = parseInt(process.argv[2]);
+console.log(factorial(myNum));
