1c9157308b15ef8ba43a74d52b9ca4fa1c92e30d
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-12-02 16:15:16 +0300

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Imports a dictionary of occurrences by user id and computes a dictionary
+ of user ids by occurrence.
+*/
+
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
