6fe078794dd9fccfbd0665e903ff47b3a2921b62
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-02 09:03:55 +0300

    javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filename = process.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(filename, 'utf-8', (error, content) => {
+fs.readFile(filePath, 'utf-8', (error, content) => {
   if (error) console.log(error);
   else console.log(content);
 });
