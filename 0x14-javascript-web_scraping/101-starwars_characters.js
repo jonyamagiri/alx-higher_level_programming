1c849d15f42e2a930c7e521897c6186e7adeedb1
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-02 14:59:56 +0300

    javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request.get(url, async (error, response, body) => {
   if (error) console.log(error);
 
-  for (const character of JSON.parse(body).characters) {
-    request.get(character, (error, response, body) => {
-      if (error) console.log(error);
-      console.log(JSON.parse(body).name);
-    });
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        request.get(character, (error, response, body) => {
+          if (error) reject(error);
+          resolve(JSON.parse(body).name);
+        });
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
