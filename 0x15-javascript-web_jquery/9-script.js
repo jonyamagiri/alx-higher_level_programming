3422d7f31cf9817783905206c1d65080a02807cf
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-11 13:53:51 +0300

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+// fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello
+$('document').ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
