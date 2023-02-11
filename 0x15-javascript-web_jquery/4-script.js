925fb0785760b9e147def2a257ac032b1dda85ee
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-11 13:18:48 +0300

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+// toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header
+$('DIV#toggle_header').click(function () {
+  $('header').toggleClass('green red');
+});
