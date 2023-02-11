e7b72c1df74a5cc5b72c90dfc4652f20e19e8a3f
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-11 13:59:08 +0300

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+// updates the text color of the <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
