07ded5e8e0ea80fdab423a23f1684f21abfe6595
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-11 13:36:45 +0300

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,4 @@
+// fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
